3bc9a16435c78c4dc771cbf67761dabdd8e0e641
Moksh Aggarwal <moksh3239.beai24@chitkara.edu.in>
2026-07-27 17:14:12 +0530

    SortingAlgos

---
+++ Sorting/selectionSortASC.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class selectionSortASC {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
