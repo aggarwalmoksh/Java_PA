ee0f759b6039f88539a79fceaa588825446a981c
Moksh Aggarwal <moksh3239.beai24@chitkara.edu.in>
2026-07-29 02:35:03 +0530

    File updated at 7/29/2026, 2:35:03 AM

--- 867-TransposeMatrix/867-TransposeMatrix.java
+++ 867-TransposeMatrix/867-TransposeMatrix.java
@@ -1,4 +1,4 @@
-// Last updated: 7/29/2026, 2:32:53 AM
+// Last updated: 7/29/2026, 2:35:03 AM
 1class Solution {
 2    public int[][] transpose(int[][] matrix) {
 3        int n = matrix.length;
@@ -6,9 +6,9 @@
 5
 6        int[][] res = new int[m][n];
 7
-8        for (int i = 0; i < matrix.length; i++) {
-9            for (int j = 0; j < matrix[0].length; j++) {
-10                res[j][i] = matrix[i][j];
+8        for (int i = 0; i < m; i++) {
+9            for (int j = 0; j < n; j++) {
+10                res[i][j] = matrix[j][i];
 11            }
 12        }
 13        return res;
