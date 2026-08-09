ae3e19e264f33603f2972ff461e8b4e8d8c8c91d
Moksh Aggarwal <moksh3239.beai24@chitkara.edu.in>
2026-08-09 20:08:17 +0530

    File updated at 8/9/2026, 8:08:17 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,18 +1,15 @@
-// Last updated: 8/9/2026, 8:06:19 PM
+// Last updated: 8/9/2026, 8:08:17 PM
 1class Solution {
-2    public int nsum(int n) {
-3        if (n==0) return 0;
-4
-5        return n + nsum(n-1);
-6    }
-7    public int missingNumber(int[] nums) {
-8        int tn = 0;
-9        for (int num : nums) {
-10            tn += num;
-11        }
-12
-13        int sum = nsum(nums.length);
-14
-15        return sum - tn;
-16    }
-17}
+2    public int missingNumber(int[] nums) {
+3        int tn = 0;
+4        for (int i =1; i <= nums.length; i++) {
+5            tn += i;
+6        }
+7
+8        for (int num : nums) {
+9            tn -= num;
+10        }
+11
+12        return tn;
+13    }
+14}
