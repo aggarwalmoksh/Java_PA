c32baf4907365116815e8f1895e8cc747293ca22
Moksh Aggarwal <moksh3239.beai24@chitkara.edu.in>
2026-09-10 14:35:26 +0530

    File updated at 9/10/2026, 2:35:25 PM

--- 848-ShiftingLetters/848-ShiftingLetters.java
+++ 848-ShiftingLetters/848-ShiftingLetters.java
@@ -1,13 +1,14 @@
-// Last updated: 9/10/2026, 3:12:18 AM
+// Last updated: 9/10/2026, 2:35:25 PM
 1class Solution {
 2    public String shiftingLetters(String s, int[] shifts) {
 3        StringBuilder sb = new StringBuilder(s);
 4        long shift = 0;
-5
-6        for (int i = s.length() - 1; i >= 0; i--) {
-7            shift += shifts[i];
-8            sb.setCharAt(i, (char)((s.charAt(i) - 'a' + shift % 26) % 26 + 'a'));
-9        }
-10        return sb.toString();
-11    }
-12}
+5        for (int i = s.length() - 1; i >= 0; i--) {
+6            shift += shifts[i];
+7            sb.setCharAt(i, 
+8                (char)((s.charAt(i) - 'a' + shift % 26) % 26 + 'a')
+9            );
+10        }
+11        return sb.toString();
+12    }
+13}
