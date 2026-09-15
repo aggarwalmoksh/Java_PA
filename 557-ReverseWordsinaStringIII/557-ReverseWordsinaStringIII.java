8c4d1c96ce12121c8e243ed24bf4466e4ca88892
Moksh Aggarwal <moksh3239.beai24@chitkara.edu.in>
2026-09-16 03:26:51 +0530

    File updated at 9/16/2026, 3:26:50 AM

--- 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
@@ -1,19 +1,13 @@
-// Last updated: 9/16/2026, 3:23:10 AM
-1import java.util.StringTokenizer;
-2
-3class Solution {
-4    public String reverseWords(String s) {
-5        StringBuilder sb = new StringBuilder();
-6        StringTokenizer st = new StringTokenizer(s);
-7
-8        while (st.hasMoreTokens()) {
-9            String word = st.nextToken();
-10            sb.append(new StringBuilder(word).reverse());
-11            if (st.hasMoreTokens()) {
-12                sb.append(" ");
-13            }
-14        }
-15
-16        return sb.toString();
-17    }
-18}
+// Last updated: 9/16/2026, 3:26:50 AM
+1class Solution {
+2    public String reverseWords(String s) {
+3        String[] words = s.split(" ");
+4        String ans = "";
+5
+6        for (String word : words) {
+7            ans += new StringBuilder(word).reverse() + " ";
+8        }
+9
+10        return ans.trim();
+11    }
+12}
