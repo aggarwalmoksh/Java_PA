7a1822cca0f03122e5407fc09c10376fc722f7d4
Moksh Aggarwal <moksh3239.beai24@chitkara.edu.in>
2026-09-16 03:56:37 +0530

    File updated at 9/16/2026, 3:56:36 AM

--- 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
@@ -1,13 +1,20 @@
-// Last updated: 9/16/2026, 3:26:50 AM
+// Last updated: 9/16/2026, 3:56:36 AM
 1class Solution {
-2    public String reverseWords(String s) {
-3        String[] words = s.split(" ");
-4        String ans = "";
-5
-6        for (String word : words) {
-7            ans += new StringBuilder(word).reverse() + " ";
-8        }
-9
-10        return ans.trim();
-11    }
-12}
+2    public String reverseStr(String s, int k) {
+3        String ans = "";
+4
+5        for (int i = 0; i < s.length(); i+=2*k) {
+6            String sub = s.substring(i, Math.min(i + 2*k, s.length()));
+7            
+8            if (sub.length() < k) {
+9                ans += new StringBuilder(sub).reverse();
+10            }
+11            else {
+12                ans += new StringBuilder(sub.substring(0, k)).reverse();
+13                ans += sub.substring(k);
+14            }
+15        }
+16
+17        return ans;
+18    }
+19}
